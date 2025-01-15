e0ba03aaf9d795792dd1b6dfbeb953cb2effb158
Thais Menezes <thais.mas@hotmail.com>
2025-01-15 12:12:47 -0300

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Thais Souza on 15/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
